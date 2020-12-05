40f270433c6025d21e957ddfb2c64270a68e8496
Chad Dotson <1754094+chaddotson@users.noreply.github.com>
2020-12-05 16:32:06 -0600

    Add exceptions

---
+++ county_adjacency/errors.py
@@ -0,0 +1,34 @@
+"""Defines possible errors encountered during county adjacency discovery."""
+from typing import List
+
+
+class CountyNotFoundError(RuntimeError):
+    """Error when specified county, district, or like was not found."""
+
+    def __init__(self, county: str, similar: List[str] = None):
+        """
+        Create the error given the county and the list of similar counties found.
+
+        :param county: The county requested
+        :param similar: The list of similar counties found
+        """
+        self.county = county
+        self.similar = similar if similar is not None else []
+
+    def __str__(self):
+        return f"""'{self.county}' not found. Perhaps you meant: '{"', '".join(self.similar)}'"""
+
+
+class NoSimilarCountiesError(RuntimeError):
+    """Error when no similar counties were found in the event of the requested county/like was not found."""
+
+    def __init__(self, county: str):
+        """
+        Create the error given the county, district, like specified.
+
+        :param county: The county requested
+        """
+        self.county = county
+
+    def __str__(self):
+        return f"No similar counties to '{self.county}' found."
