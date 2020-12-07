4ac0f1577ff508cc9889ce74e5051557d6768bea
Chad Dotson <1754094+chaddotson@users.noreply.github.com>
2020-12-06 20:14:37 -0600

    Update init with valid errors.

--- src/county_adjacency/__init__.py
+++ src/county_adjacency/__init__.py
@@ -1,7 +1,8 @@
 """Provides adjacent county information."""
 
 from county_adjacency.adjacency import get_neighboring_counties, supported_counties
+from county_adjacency.errors import CountyNotFoundError, NoSimilarCountiesError
 
-__all__ = ["get_neighboring_counties", "supported_counties"]
+__all__ = ["CountyNotFoundError", "get_neighboring_counties", "NoSimilarCountiesError", "supported_counties"]
 
 __version__ = "0.1.1"
